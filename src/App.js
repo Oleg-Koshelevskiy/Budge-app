b45a877011b7a9b1b2b1b4264b4693b9003440d7
Oleg <oleg17.k@gmail.com>
2023-01-15 18:09:36 +0200

    mainly styled header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import "./App.module.css";
-import Container from "./components/UI/Container";
+import "./App.css";
 import Header from "./layout/Header";
 import Main from "./layout/Main";
 import Footer from "./layout/Footer";
 
 function App() {
   return (
-    <Container>
+    <>
       <Header />
       <Main />
       <Footer />
-    </Container>
+    </>
   );
 }
 
